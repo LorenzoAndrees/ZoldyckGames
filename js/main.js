61646430bd463f107c839e900f4c8e1fa80d302f
LorenzoAndrees <lordandrees@gmail.com>
2019-06-25 02:25:06 -0400

    + Actualizaciones

--- js/main.js
+++ js/main.js
@@ -432,67 +432,6 @@
 
 		}
 	};
-
-	var clickqueries = function () {
-
-		// Get the modal
-		var modal = document.getElementById("modalqueries");
-
-		// Get the button that opens the modal
-		var btn = document.getElementById("btn1");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks on the button, open the modal 
-		btn.onclick = function () {
-			modal.style.display = "block";
-		}
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function () {
-			modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function (event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-	};
-
-	var clickmanagment = function () {
-
-		// Get the modal
-		var modal = document.getElementById("modalmanagment");
-
-		// Get the button that opens the modal
-		var btn = document.getElementById("btn2");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks on the button, open the modal 
-		btn.onclick = function () {
-			modal.style.display = "block";
-		}
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function () {
-			modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function (event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-	};
-
-
-
 	// Document on load.
 	$(function () {
 
@@ -518,10 +457,7 @@
 		aboutAnimate();
 		countersAnimate();
 		contactAnimate();
-
-		clickqueries();
-		clickmanagment();
-		clicklaunch();
+		
 	});
 
 }());
